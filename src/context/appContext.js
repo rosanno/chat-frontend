0c6642629bd3b0970ce4502a49d50007c4c3ffe6
rosanno <rosannomacabuhay@gmail.com>
2022-10-12 20:33:33 +0800

    remove transport option in socket.io

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -2,8 +2,6 @@ import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
 const SOCKET_URL = import.meta.env.VITE_APP_BASE_URL;
-export const socket = io(SOCKET_URL, {
-  transports: ['websocket'],
-});
+export const socket = io(SOCKET_URL);
 
 export const AppContext = createContext();
