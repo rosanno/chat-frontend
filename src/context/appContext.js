c6b12ee8ddb226796202d881fe59fa13d286f0bc
rosanno <rosannomacabuhay@gmail.com>
2022-10-12 20:22:12 +0800

    add transports options in socket.io

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -2,6 +2,8 @@ import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
 const SOCKET_URL = import.meta.env.VITE_APP_BASE_URL;
-export const socket = io(SOCKET_URL);
+export const socket = io(SOCKET_URL, {
+  transports: ['websocket'],
+});
 
 export const AppContext = createContext();
